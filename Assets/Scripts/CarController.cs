3266f97d9c82e8abeb56c5a8c5f38d785c25fbe1
Imeguras <imeguras@teknik.io>
2022-11-22 13:52:52 +0000

    Simple ackerman stuff

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,56 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.Ackermann;
+using System.IO;
 
-public class CarController : MonoBehaviour
-{
+public class CarController : MonoBehaviour{
     ROSConnection ros;
     public string controlTopicName = "/ackermann_control";
     public double wheelbase = 1550; // in cm
     public double cogToRear = 0.775; // center of gravity to rear wheel axis
     public double mass = 270; // in kg
-
-    private double speed = 0;
     public double maxSpeed = 10; // m/s
-    private double steeringAngle = 0;
+    public double steeringAngle = 0;
+	public double motorTorque=80; //in nm
     public double maxSteeringAngle = 40; // +/-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        ROSConnection.GetOrCreateInstance().Subscribe<AckermannDriveMsg>(controlTopicName, Control);
-    }
-
-    void Control(AckermannDriveMsg msg)
-    {
-        this.speed = msg.speed;
-        this.steeringAngle = msg.steering_angle;
-    }
-
-    void Update() 
-    {
+	private double ackermannRatio=1.1; 
+    public WheelCollider[] backWheels = new WheelCollider[2];
+	public WheelCollider[] frontWheels = new WheelCollider[2];
+	
+	void Start(){
+		//get rigidbody and set mass to this mass
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.mass = (float)mass;
+		foreach (var item in backWheels){
+			//give the wheels the motor torque
+			item.motorTorque = (float)motorTorque;
+	    }
+	}
+    void Control(AckermannDriveMsg msg){
+		
+	}
+	void FixedUpdate(){
+		Steer(steeringAngle); 
+		
+	}
+    void Update(){
         
     }
+	//simplification of it but im sleepy and it werks for now
 
-    public double getSpeed()
-    {
-        return this.speed;
-    }
+	void Steer(double angle=0){
+		if(angle>0){
+			frontWheels[0].steerAngle=(float)(angle); 
+			frontWheels[1].steerAngle=(float)(angle*ackermannRatio);
+		}else{
+			frontWheels[0].steerAngle=(float)(angle*ackermannRatio); 
+			frontWheels[1].steerAngle=(float)(angle);
+		}
+	}
 
-    public void setSpeed(double speed)
-    {
-        if(speed <= this.maxSpeed)
-            this.speed = speed;
-    }
-
-    public double getSteeringAngle()
-    {
-        return this.steeringAngle;
-    }
-
-    public void setSteeringAngle(double steeringAngle)
-    {
-        if(steeringAngle <= this.maxSteeringAngle && steeringAngle >= -this.maxSteeringAngle)
-            this.steeringAngle = steeringAngle;
-    }
 }
